c2fcb41b42798b14dda56ff1d24508a584b0acda
JunSeo99 <an001209@gmail.com>
2025-06-18 23:50:01 +0900

    first commit

---
+++ Tests/computer-network-homework-serverTests/computer-network-homework-serverTests.swift
@@ -0,0 +1,19 @@
+@testable import computer-network-homework-server
+import VaporTesting
+import Testing
+
+
+@Suite("App Tests")
+
+struct computer-network-homework-serverTests {
+    @Test("Test Hello World Route")
+    func helloWorld() async throws {
+        try await withApp(configure: configure) { app in
+            try await app.testing().test(.GET, "hello", afterResponse: { res async in
+                #expect(res.status == .ok)
+                #expect(res.body.string == "Hello, world!")
+            })
+        }
+    }
+}
+
